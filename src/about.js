7754ea547181eada174f25a0ec2fc462448f7094
drorkrief <770mdk@gmail.com>
2021-04-29 17:02:20 +0300

    add style 3

--- src/about.js
+++ src/about.js
@@ -1,2 +1,6 @@
-const About = () => <h1>About</h1>;
-export default About
+const About = () => (
+  <div>
+    <h1>About</h1>
+  </div>
+);
+export default About;
--- src/zmanim.js
+++ src/zmanim.js
@@ -2,63 +2,86 @@ import React from "react";
 import axios from "axios";
 import { Table } from "react-bootstrap";
 
-class Zmanim extends React.Component {
-  state={
-    times:false
-  }
-  componentDidMount() {
-    axios
-      .get("https://www.hebcal.com/shabbat?cfg=json&geonameid=281184&M=on&lg=h")
-      .then((res) => {
-        const persons = res.data;
-
-        this.setState({
-          times: persons,
-        });
-      });
-  }
-  render() {
-    if(this.state.times){
-      let results = this.state.times.items.filter((x)=>x.category === "candles" );
-      console.log(results[0].title);
+const tableDataHandle = (objectData) => {
+  let itemToReturn = {};
+  for (let item of objectData.items) {
+    if (item.category === "candles") {
+      itemToReturn.candles = item.title;
     }
+    if (item.category === "havdalah") {
+      itemToReturn.havdalah = item.title;
+    }
+    if (item.category === "parashat") {
+      itemToReturn.parashat = item.hebrew;
+    }
+  }
+  return itemToReturn;
+};
+class Zmanim extends React.Component {
+  state = {
+    times: false,
+    arrayvar: [],
+    cities: ["תל-אביב", "באר-שבע", "חיפה", "ירושלים"],
+  };
+  componentDidMount() {
+    // 293397 - tel aviv geoNameId
+    // Be'er Sheva|295530 geoNameId
+    // Haifa|294801 geoNameId
+    // geonameid=281184 jerusalem geoNameId
+    let locations = ["293397", "295530", "294801", "281184"];
+    for (let geonameid of locations) {
+      axios
+        .get(
+          `https://www.hebcal.com/shabbat?cfg=json&geonameid=${geonameid}&M=on&lg=h`
+        )
+        .then((res) => {
+          const persons = res.data;
+          this.setState((prevState) => ({
+            arrayvar: [...prevState.arrayvar, tableDataHandle(persons)],
+          }));
+        });
+    }
+  }
+
+  render() {
     return (
       <div className="container">
-        <h1>זמני שבת בעברית</h1>
+        <h1 className="date">זמני שבת בעברית</h1>
+        <p className="date">
+          <span>היום </span>
+          <script
+            type="text/javascript"
+            charSet="utf-8"
+            src="https://www.hebcal.com/etc/hdate-he.js"
+          ></script>
+          י״ז בְּאִיָיר תשפ״א
+        </p>
         {}
-        <Table striped bordered hover>
-  <thead>
-    <tr>
-      <th colSpan="3">פרשת: </th>
-    </tr>
-    <tr>
-      <th>הדלקת נרות</th>
-      <th>צאת השבת</th>
-      <th>עיר</th>
-      
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td>1</td>
-      <td>Mark</td>
-      <td>Otto</td>
-      
-    </tr>
-    <tr>
-      <td>2</td>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      
-    </tr>
-    <tr>
-      <td>3</td>
-      <td>3</td>
-      <td>3</td>
-
-    </tr>
-  </tbody>
-</Table>
+        <Table striped bordered hover className="tabled">
+          <thead>
+            <tr>
+              <th colSpan="3">
+                {this.state.arrayvar.length > 0 &&
+                  this.state.arrayvar[0].parashat}
+              </th>
+            </tr>
+            <tr>
+              <th>צאת השבת</th>
+              <th>הדלקת נרות</th>
+              <th>עיר</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.arrayvar.map((x, i) => (
+              <tr>
+                <td>{x.havdalah.replace("הַבדָלָה:","")}</td>
+                <td>{x.candles.replace("הַדלָקָת נֵרוֹת:","")}</td>
+                <td>{this.state.cities[i]}</td>
+              </tr>
+            ))}
+           
+          </tbody>
+        </Table>
       </div>
     );
   }
